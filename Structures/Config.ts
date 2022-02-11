6e27369feff536f1dfd79893cc0c71585df5e4db
Logic <80296087+Logic-Mastermind@users.noreply.github.com>
2022-02-11 00:06:48 +0000

    sync changes

--- Structures/Config.ts
+++ Structures/Config.ts
@@ -1,6 +1,6 @@
 const Config = {
-  token: "ODczNTI3MDc2MzU4MDY2MTk2.YQ5tfA.5T-dnoAS_D1HkoXGIyJU30NXXVQ" /*process.env["TOKEN"]*/,
-  pasteBinAPI: "f5fbwrkCQc_JgVjbquM3FdnhlaIpZL7_" /*process.env["PASTE_BIN_API"]*/,
+  token: process.env["TOKEN"],
+  pasteBinAPI: process.env["PASTE_BIN_API"],
   globalTicketLimit: 3
 }
 
